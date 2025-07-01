f2aa38366a8aa980de490cace35cf8c9766b1cbf
Fahim Ahmed <fahim52510@gmail.com>
2025-07-02 00:00:53 +0600

    feat: integrate Clerk authentication middleware and add protected route for user data retrieval

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express, { Express, Request, Response } from "express";
 import apiRouter from "./routes";
 import cors from "cors";
 import path from "path";
-
+import { clerkMiddleware } from "@clerk/express";
+import { clerkClient, requireAuth, getAuth } from "@clerk/express";
 const app: Express = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(clerkMiddleware());
 app.use(express.json());
 
 app.set("view engine", "ejs");
@@ -18,6 +20,26 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
+const asyncHandler = (
+  fn: (req: Request, res: Response, next: express.NextFunction) => Promise<any>
+): express.RequestHandler => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
+app.get(
+  "/api/v1/protected",
+  requireAuth(),
+  asyncHandler(async (req, res) => {
+    // Use `getAuth()` to get the user's `userId`
+    const { userId } = getAuth(req);
+    console.log(userId);
+    // Use Clerk's JavaScript Backend SDK to get the user's User object
+    const user = await clerkClient.users.getUser(userId);
+
+    return res.json({ user });
+  })
+);
 app.use("/api", apiRouter);
 
 app.listen(port, () => {
